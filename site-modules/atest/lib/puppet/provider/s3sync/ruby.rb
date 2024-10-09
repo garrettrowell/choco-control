15fd47435a5276594574fef5fd8eb709a86d6c5a
Garrett Rowell <garrett@puppet.com>
2024-10-09 13:03:53 -0600

    try

--- site-modules/atest/lib/puppet/provider/s3sync/ruby.rb
+++ site-modules/atest/lib/puppet/provider/s3sync/ruby.rb
@@ -28,9 +28,10 @@ Puppet::Type.type(:s3sync).provide(:ruby) do
     if File.directory?(resource[:localpath]) || File.exist?(resource[:localpath])
       # If the directory or file exists we need to check if what we have locally is insync with whats in the bucket
       # if dry_run returns an empty array, we are in sync
-      result = dry_run(resource[:bucket], resource[:localpath], resource[:connect_timeout], resource[:region]).empty?
-      Puppet.info(".exists? dry_run result: #{result}")
-      result
+#      result = dry_run(resource[:bucket], resource[:localpath], resource[:connect_timeout], resource[:region]).empty?
+#      Puppet.info(".exists? dry_run result: #{result}")
+#      result
+      true
     else
       Puppet.info('.exists? file/dir not there')
       false
@@ -39,7 +40,10 @@ Puppet::Type.type(:s3sync).provide(:ruby) do
 
   def localpath
     Puppet.info('in localpath getter')
-    self.exists?
+    result = dry_run(resource[:bucket], resource[:localpath], resource[:connect_timeout], resource[:region]).empty?
+    Puppet.info(".exists? dry_run result: #{result}")
+    result
+#    self.exists?
   end
 
   def localpath=(value)
