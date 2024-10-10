f119b02bee5b3adf3ca532f8a5d3283c2aee71f9
Garrett Rowell <garrett@puppet.com>
2024-10-10 16:20:59 -0600

    asdf

--- site-modules/atest/lib/puppet/provider/s3sync/ruby.rb
+++ site-modules/atest/lib/puppet/provider/s3sync/ruby.rb
@@ -48,10 +48,11 @@ Puppet::Type.type(:s3sync).provide(:ruby) do
 
   def exists?
     if File.directory?(resource[:localpath]) || File.exist?(resource[:localpath])
+      # Only run the dry_run when ensuring present. If we're ensuring absent... we've already determined it's here
       # If the directory or file exists we need to check if what we have locally is insync with whats in the bucket
       # if dry_run returns an empty array, we are in sync
-      result = dry_run.empty?
-      Puppet.info(".exists? dry_run result: #{result}")
+      result = resource[:ensure].eq(:present) ? dry_run.empty? : true
+      Puppet.info(".exists? result: #{result}")
       result
     else
       Puppet.info('.exists? file/dir not there')
